c1cb52353f2a8639f422ea9c730be24d622766c5
Your Name <you@example.com>
2023-02-22 13:15:13 -0600

    link apps

--- dashboard/vistas/parte_superior.php
+++ dashboard/vistas/parte_superior.php
@@ -17,41 +17,19 @@ if($_SESSION["s_usuario"] === null){
 
 
 <!DOCTYPE html>
-
 <html lang="en">
-
-
-
 <head>
 
-
-
   <meta charset="utf-8">
-
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
   <meta name="description" content="">
-
   <meta name="author" content="">
-
-
-
   <title>Csn Cooperativa</title>
-
-
-
   <!-- Custom fonts for this template-->
-
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-
-
   <!-- Custom styles for this template-->
-
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
 
@@ -64,18 +42,9 @@ if($_SESSION["s_usuario"] === null){
 
     <link rel="stylesheet"  type="text/css" href="vendor/datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
 
-
-
 </head>
-
-
-
 <body id="page-top">
-
-
-
   <!-- Page Wrapper -->
-
   <div id="wrapper">
 
 
@@ -156,15 +125,15 @@ if($_SESSION["s_usuario"] === null){
 
             <h6 class="collapse-header">Aplicaciones Disponibles:</h6>
 
-            <a class="collapse-item" href="vistas/app1/index.php">App demo1</a>
+            <a class="collapse-item" href="https://apps-demo1-csn-login.apps.oc.4ks.online">App demo1</a>
             
-            <a class="collapse-item" href="vistas/app2/index.php">App demo2</a>
+            <a class="collapse-item" href="https://apps-demo2-csn-login.apps.oc.4ks.online">App demo2</a>
             
-            <a class="collapse-item" href="vistas/app3/index.php">App demo3</a>
+            <a class="collapse-item" href="https://apps-demo3-csn-login.apps.oc.4ks.online">App demo3</a>
             
-            <a class="collapse-item" href="vistas/app4/index.php">App demo4</a>
+            <a class="collapse-item" href="https://apps-demo4-csn-login.apps.oc.4ks.online">App demo4</a>
             
-            <a class="collapse-item" href="vistas/app5/index.php">App demo5</a>
+            <a class="collapse-item" href="https://apps-demo5-csn-login.apps.oc.4ks.online">App demo5</a>
 
           </div>
 
